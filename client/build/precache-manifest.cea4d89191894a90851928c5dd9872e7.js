ef7c1109de5273516d19af88ff39bff8e5c132e3
mharshita <manglaharshita@gmail.com>
2021-01-11 22:34:24 +0530

    build

---
+++ client/build/precache-manifest.cea4d89191894a90851928c5dd9872e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0c9e86223a545541a5648ca21de2d46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5420b6df57a19548f0b2",
+    "url": "/static/css/main.98346d44.chunk.css"
+  },
+  {
+    "revision": "f7d53b4bdbcb13fde373",
+    "url": "/static/js/2.7dddd35a.chunk.js"
+  },
+  {
+    "revision": "5420b6df57a19548f0b2",
+    "url": "/static/js/main.6ab14ebb.chunk.js"
+  },
+  {
+    "revision": "20dcb6334915f836950a",
+    "url": "/static/js/runtime~main.00070426.js"
+  }
+]);
